84257b8053b638bcefb738724c9847f76c6bf0e0
Rubenperes <88197615+Rubenperes@users.noreply.github.com>
2024-11-22 22:15:06 +0100

    ♻️ Changement API

--- projet/app/services/gen_img.py
+++ projet/app/services/gen_img.py
@@ -4,31 +4,65 @@ from typing import Dict, Union, List
 from fastapi import Depends, HTTPException, Header
 from sqlalchemy.orm import Session
 import requests
+import time
 
 import app.models as models
 from app.schemas.users import User
 
-LIMEWARE_API_KEY = os.getenv("LIMEWARE_API_KEY")
+STARRYAI_API_KEY = os.getenv("STARRYAI_API_KEY")
 
-def gen_image(prompt):
-    url = "https://api.limewire.com/api/image/generation"
-
-    payload = {
-    "prompt": prompt,
-    "aspect_ratio": "1:1"
-    }
+def get_image(id):
+    url = f"https://api.starryai.com/creations/{id}"
 
     headers = {
-    "Content-Type": "application/json",
-    "X-Api-Version": "v1",
-    "Accept": "application/json",
-    "Authorization": f"Bearer {LIMEWARE_API_KEY}"
+        "accept": "application/json",
+        "X-API-Key": STARRYAI_API_KEY
+    }
+
+    response = requests.get(url, headers=headers)
+
+    return response.json()
+
+def gen_image(prompt):
+    url = "https://api.starryai.com/creations/"
+
+    payload = {
+        "model": "lyra",
+        "aspectRatio": "square",
+        "highResolution": False,
+        "images": 1,
+        "steps": 20,
+        "initialImageMode": "color",
+        "prompt": prompt
+    }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "X-API-Key": STARRYAI_API_KEY
     }
 
     response = requests.post(url, json=payload, headers=headers)
-    
+
     data = response.json()
-    
-    print(f"data : {data}")
-    
-    return data
+
+    id = data["id"]
+
+    response = get_image(id)
+
+    status = response["status"]
+
+    print(f"status : {status}")
+
+    while status != "completed":
+        time.sleep(25)
+        response = get_image(id)
+        status = response["status"]
+        print(f"status : {status}")
+
+    print(response)
+
+    url = response["images"][0]["url"]
+
+    print(f"url : {url}")
+
+    return url
